fbf77dce1c2b7dbbd51e39bb52093082380fc9ca
20023841 <20023841@myrp.edu.sg>
2021-05-10 22:04:04 +0800

    worksheet activity 2 update

--- app/src/main/java/sg/edu/rp/c346/id20023841/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20023841/democheckboxexample/MainActivity.java
@@ -3,10 +3,12 @@ package sg.edu.rp.c346.id20023841.democheckboxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,13 +28,21 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("MainActivity", "Inside onClick");
                 if(cb.isChecked()){
-                    tv.setText("You have your discount!");
+                    double pay = calcPay(100,20);
+                    tv.setText("Discount given, pay " + pay);
                 }
                 else{
-                    tv.setText("No discount for you!");
+                    double pay = calcPay(100, 0);
+                    tv.setText("No discount, pay " + pay);
                 }
+                Toast.makeText(MainActivity.this, "Are you happy?", Toast.LENGTH_LONG).show();
             }
         });
+
+    }
+    private double calcPay(double price, double discount){
+        return price * (1 - discount/100);
     }
 }
